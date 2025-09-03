36324fc62154dde2bf8c93e4fc01f0bd979bdd78
zunerz671 <zunerz671@gmail.com>
2025-09-03 18:58:32 +1000

    w,r,a program

--- file-io.rb
+++ file-io.rb
@@ -1,10 +1,42 @@
+# begin
+#   file_name = "example.txt"
+#   file = File.open("#{file_name}", "r")
+#   contents = file.read
+#   puts contents
+# rescue Errno::ENOENT
+#   puts "File name: '#{file_name}', not found."
+# ensure
+#   file.close if file 
+# end
+
+# -------
+
+# Exercise:
+# Task: Write user input to a file.
+# Instructions:
+# Prompt the user for their name and a message.
+# Write this information to a file named user_messages.txt (do not create this file manually).
+# Ensure proper error handling and closing of the file.
+# Test it out and see how it created a new file named user_messages.txt with that message.
+# Test it out again and see how it deletes everything in user_messages.txt and adds the new message.
+# Now that you’ve seen that ‘w’ overwrites everything, now change ‘w’ to ‘a’ for append and test it again to see how it appends to the end of the file rather than overwriting it.
+# Test it multiple times to see how it continues to add to the file rather than delete everything.
+
+puts "Select w, r, or a"
+selection = gets.chomp
+
+puts "Please provide your name:"
+name = gets.chomp
+
+puts "Please provide a message:"
+message = gets.chomp
+
 begin
-  file_name = "example.txt"
-  file = File.open("#{file_name}", "r")
-  contents = file.read
-  puts contents
-rescue Errno::ENOENT
-  puts "File name: '#{file_name}', not found."
-ensure
-  file.close if file 
+  file = File.open("user_messages.txt", "#{selection}")
+  file.puts "Your name: #{name}."
+  file.puts "Your message: #{message}."
+
+  puts "Info input was successful."
+rescue
+  puts "An error occured"
 end
